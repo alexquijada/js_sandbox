68a0021b1434e36d976d4ac467259ef088fb000c
Alex Quijada <aquijad@gmail.com>
2018-06-24 18:49:55 -0400

    js changes

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-alert('hey! work already!');
+var name = 'alex quijada';
+console.log(name);
+name = 'javascript sandbox';
+console.log(name);
+
+//multi word variables
+var firstName ='Alex'; //Camel case
+var first_name = 'Justin'; //Underscore
+var FirstName = 'Jack'; //Pascal case
 
