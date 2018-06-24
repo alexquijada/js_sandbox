fff7aa9e93dce3f38212cb068d7f5a0af947ed98
Alex Quijada <aquijad@gmail.com>
2018-06-24 19:09:31 -0400

    js changes

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
-var name = 'alex quijada';
-console.log(name);
-name = 'javascript sandbox';
-console.log(name);
+// var name = 'alex quijada';
+// console.log(name);
+// name = 'javascript sandbox';
+// console.log(name);
 
-//multi word variables
-var firstName ='Alex'; //Camel case
-var first_name = 'Justin'; //Underscore
-var FirstName = 'Jack'; //Pascal case
+// //multi word variables
+// var firstName ='Alex'; //Camel case
+// var first_name = 'Justin'; //Underscore
+// var FirstName = 'Jack'; //Pascal case
+
+// LET
+// let name;
+// name = 'alex quijada';
+// console.log(name);
+// name = 'javascript sandbox';
+// console.log(name);
+
+// CONST
+// const name = 'Alex';
+// console.log=(name);
+
+// CONST Value
+
+// const person = {
+//   name:'alex',
+//   age: 28
+// }
+
+// console.log(person);
 
